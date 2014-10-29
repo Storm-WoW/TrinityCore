a701c932e5fe2c84ce0198251081ac7aa4236fdb
Tangol <traian4life@yahoo.com>
2014-10-29 18:40:13 +0200

    [DB][Spells] Fixes Blood-Queen Lana'thel Uncontrollable Frenzy exploit (part II)

---
+++ sql/storm/world/uncontrollable_frenzy_fix.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `spell_id` IN (70924,73015);
+INSERT INTO `spell_script_names` VALUES ('70924', 'spell_blood_queen_uncontrollable_frenzy');
+INSERT INTO `spell_script_names` VALUES ('73015', 'spell_blood_queen_uncontrollable_frenzy');
