509cc5a15958bd04f21cf48085dbcd77749de685
Tangol <traian4life@yahoo.com>
2014-11-03 23:30:56 +0200

    [DB][Creature] Some additions to https://github.com/Storm-WoW/TrinityCore/commit/8caa84c8b1d2412cf1e179d91788da2673157886

--- sql/storm/world/dungeon_boss_immunities.sql
+++ sql/storm/world/dungeon_boss_immunities.sql
@@ -20,40 +20,83 @@ UPDATE `creature_template` SET `mechanic_immune_mask`=mechanic_immune_mask
 |67108864 -- DAZE
 |536870912 -- SAPPED
 WHERE `entry` IN
-(32273,32313, -- Infinite Corruptor
-30258,31463, -- Amanitar
-29932, -- Eck the Ferocious
-29573,31367, -- Drakkari Elemental
-29316,31510, -- Moragg
-29315,31507, -- Erekem
-31134,31506, -- Cyanigosa
-29314,31512, -- Zuramat the Obliterator
-29313,31508, -- Ichoron
-29312,31509, -- Lavanthor
-29311,31464, -- Herald Volazj
-29310,31465, -- Jedoga Shadowseeker
-29308,31469, -- Prince Taldaram
-29307,31365, -- Drakkari Colossus
-29306,31368, -- Gal'darah
-29305,30530, -- Moorabi
-29304,31370, -- Slad'ran
-29266,31511, -- Xevozz
-28923,31538, -- Loken
-28921,31611, -- Hadronox
-28587,31536, -- Volkhan
-28586,31533, -- General Bjarngrim
-28546,31537, -- Ionar
-27978,31386, -- Sjonnir The Ironshaper
-27977,31381, -- Krystallus
-27975,31384, -- Maiden of Grief
-27656,31561, -- Ley-Guardian Eregos
-27655,31560, -- Mage-Lord Urom
-27654,31558, -- Drakos the Interrogator
-27447,31559, -- Varos Cloudstrider
+(23953,30748, -- Prince Keleseth
 27390,31680, -- Skarvald the Constructor
 27389,31657, -- Dalronn the Controller
-26687,30774, -- Gortok Palehoof
-26533,31217, -- Mal'Ganis
-26532,31215, -- Chrono-Lord Epoch
+23954,31673, -- Ingvar the Plunderer
+
+-- The Nexus
+26731,30510, -- Grand Magus Telestra
+26763,30529, -- Anomalus
+26794,30532, -- Ormorok the Tree-Shaper
+26723,30540, -- Keristrasza
+26796,30398, -- Commander Stoutbeard
+26798,30397, -- Commander Kolurg
+
+-- Azjol-Nerub
+28684,31612, -- Krik'thir the Gatewatcher
+28921,31611, -- Hadronox
+29120,31610, -- Anub'arak
+
+-- Ahn'kahet: The Old Kingdom
+29309,31456, -- Elder Nadox
+29308,31469, -- Prince Taldaram
+29310,31465, -- Jedoga Shadowseeker
+29311,31464, -- Herald Volazj
+30258,31463, -- Amanitar
+
+-- Drak'Tharon Keep
+26630,31362, -- Trollgore
+26631,31350, -- Novos the Summoner
+27483,31349, -- King Dred
+26632,31360, -- The Prophet Tharon'ja
+
+-- The Violet Hold
+29315,31507, -- Erekem
+29316,31510, -- Moragg
+29313,31508, -- Ichoron
+29266,31511, -- Xevozz
+29312,31509, -- Lavanthor
+29314,31512, -- Zuramat the Obliterator
+31134,31506, -- Cyanigosa
+
+-- Gundrak
+29304,31370, -- Slad'ran
+29307,31365, -- Drakkari Colossus
+29573,31367, -- Drakkari Elemental
+29305,30530, -- Moorabi
+29306,31368, -- Gal'darah
+29932, -- Eck the Ferocious
+
+-- Halls of Stone
+27975,31384, -- Maiden of Grief
+27977,31381, -- Krystallus
+27978,31386, -- Sjonnir The Ironshaper
+
+-- The Culling of Stratholme
+26529,31211, -- Meathook
 26530,31212, -- Salramm the Fleshcrafter
-27483,31349); -- King Dred
+26532,31215, -- Chrono-Lord Epoch
+26533,31217, -- Mal'Ganis
+32273,32313, -- Infinite Corruptor
+
+-- Halls of Lightning
+28586,31533, -- General Bjarngrim
+28587,31536, -- Volkhan
+28546,31537, -- Ionar
+28923,31538, -- Loken
+
+-- The Oculus
+27654,31558, -- Drakos the Interrogator
+27447,31559, -- Varos Cloudstrider
+27655,31560, -- Mage-Lord Urom
+27656,31561, -- Ley-Guardian Eregos
+
+-- Utgarde Pinnacle
+26668,30810, -- Svala Sorrowgrave
+26687,30774, -- Gortok Palehoof
+26693,30807, -- Skadi the Ruthless
+26861,30788, -- King Ymiron
+
+-- The Forge of Souls
+36497,36498); -- Bronjahm
